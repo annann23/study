8a00b008e222b7b38eac0063c5b7f62f02c99118
jungwunkim <annann23@naver.com>
2026-06-13 12:32:00 +0900

    차가운커피 hot으로 되어있던 오류 수정

--- 4/4-1/vendingmachine/src/main/java/com/example/vendingmachine/ProductType.java
+++ 4/4-1/vendingmachine/src/main/java/com/example/vendingmachine/ProductType.java
@@ -14,7 +14,7 @@ public record ProductType (
     public static final ProductType ZERO_COKE = new ProductType("제로콜라", 1500, 355, Temperature.COLD, Container.CAN);
     public static final ProductType GREEN_TEA = new ProductType("녹차", 1000, 500, Temperature.COLD, Container.PET_BOTTLE);
     public static final ProductType HOT_COFFEE = new ProductType("칸타타", 1400, 400, Temperature.HOT, Container.CAN);
-    public static final ProductType COLD_COFFEE = new ProductType("레쓰비", 1000, 175, Temperature.HOT, Container.CAN);
+    public static final ProductType COLD_COFFEE = new ProductType("레쓰비", 1000, 175, Temperature.COLD, Container.CAN);
     public static final ProductType WATER = new ProductType("물", 600, 500, Temperature.COLD, Container.PET_BOTTLE);
     public static final ProductType MORNING_SUNLIGHT = new ProductType("아침햇살", 900, 480, Temperature.COLD, Container.PET_BOTTLE);
     public static final ProductType ALOE_JUICE = new ProductType("알로에주스", 1000, 480, Temperature.COLD, Container.PET_BOTTLE);
