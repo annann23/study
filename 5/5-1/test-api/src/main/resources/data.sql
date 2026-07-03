4e65bc1ff54e1a0c9d9a4901b5953ef1452e7fd5
jungwunkim <annann23@naver.com>
2026-07-03 14:49:04 +0900

    유저 레벨 초기값 설정

---
+++ 5/5-1/test-api/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_level (name) VALUES ('일반회원');
+INSERT INTO user_level (name) VALUES ('VIP회원');
+INSERT INTO user_level (name) VALUES ('VVIP회원');
+INSERT INTO user_level (name) VALUES ('관리자');
